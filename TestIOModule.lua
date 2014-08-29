49100b5e408d737751e2ffc9fe5ebfc5fcc4335a
ArturMalyszewicz <amalyszewicz@pl.sii.eu>
2014-08-29 12:19:31 +0000

    first TC related to IOs, just a draft version to get familiar with this area

---
+++ TestIOModule.lua
@@ -0,0 +1,174 @@
+--[[
+
+  TestIOModule.lua
+
+  Test main.lua module
+
+--  Module :      $URL: https://ott-svn1.skywavemobile.com/svn/M2M/Agents/Source/J1939/trunk/featureTests/TestMainModule.lua $
+--  Revision:     $Revision: 1226 $
+--  Last Updated By:  $Author: shawn_l $
+--  Last Updated:   $Date: 2014-04-01 09:19:40 -0400 (Tue, 01 Apr 2014) $
+
+
+]]
+
+
+local cfg, framework, gateway, lsf, device, gps = require "TestFramework"()
+local lunatest              = require "lunatest"
+local bit                   = require "bit"
+local avlMessagesMINs       = require("MessagesMINs")           -- the MINs of the messages are taken from the external file
+local avlPopertiesPINs      = require("PropertiesPINs")         -- the PINs of the properties are taken from the external file
+local avlHelperFunctions    = require "avlHelperFunctions"()    -- all AVL Agent related functions put in avlHelperFunctions file
+local avlAgentCons          = require("AvlAgentCons")
+
+-- global variables used in the tests
+gpsReadInterval   = 1 -- used to configure the time interval of updating the position , in seconds
+
+-------------------------
+-- Setup and Teardown
+-------------------------
+
+--- suite_setup function ensures that terminal is not in the moving state and not in the low power mode
+ -- executed before each test suite
+ -- * actions performed:
+ -- lpmTrigger is set to 0 so that nothing can put terminal into the low power mode
+ -- function checks if terminal is not the low power mode (condition necessary for all GPS related test cases)
+ -- *initial conditions:
+ -- running Terminal Simulator with installed AVL Agent, running Modem Simulator with Gateway Web Service and
+ -- GPS Web Service switched on
+ -- *Expected results:
+ -- lpmTrigger set correctly and terminal is not in the Low Power mode
+function suite_setup()
+
+ -- setting lpmTrigger to 0 (nothing can put terminal into the low power mode)
+  lsf.setProperties(avlAgentCons.avlAgentSIN,{
+                                              {avlPropertiesPINs.lpmTrigger, 0},
+                                             }
+                    )
+  -- checking the terminal state
+  local avlStatesProperty = lsf.getProperties(avlAgentCons.avlAgentSIN,avlPropertiesPINs.avlStates)
+  assert_false(avlHelperFunctions.stateDetector(avlStatesProperty).InLPM, "Terminal is incorrectly in low power mode")
+
+end
+
+
+-- executed after each test suite
+function suite_teardown()
+
+-- nothing here for now
+
+end
+
+--- the setup function puts terminal into the stationary state and checks if that state has been correctly obtained
+  -- it also sets gpsReadInterval (in position service) to the value of gpsReadInterval
+  -- executed before each unit test
+  -- *actions performed:
+  -- setting of the gpsReadInterval (in the position service) is made using global gpsReadInterval variable
+  -- function sets stationaryDebounceTime to 1 second, stationarySpeedThld to 5 kmh and simulated gps speed to 0 kmh
+  -- then function waits until the terminal get the non-moving state and checks the state by reading the avlStatesProperty
+  -- *initial conditions:
+  -- terminal not in the low power mode
+  -- *expected results:
+  -- terminal correctly put in the stationary state
+function setup()
+
+  lsf.setProperties(20,{
+                        {15,gpsReadInterval}     -- setting the continues mode of position service (SIN 20, PIN 15)
+                                                 -- gps will be read every gpsReadInterval (in seconds)
+                      }
+                    )
+
+  local stationaryDebounceTime = 1      -- seconds
+  local stationarySpeedThld = 5         -- kmh
+  -- gps settings table
+  local gpsSettings={
+              speed = 0,
+              fixType=3
+                     }
+
+  --setting properties of the service
+  lsf.setProperties(avlAgentCons.avlAgentSIN,{
+                                              {avlPropertiesPINs.stationarySpeedThld, stationarySpeedThld},
+                                              {avlPropertiesPINs.stationaryDebounceTime, stationaryDebounceTime}
+                                             }
+                    )
+
+  -- set the speed to zero and wait for stationaryDebounceTime to make sure the moving state is false
+  gps.set(gpsSettings) -- applying settings of gps simulator
+  framework.delay(stationaryDebounceTime+gpsReadInterval+3) -- three seconds are added to make sure the gps is read and processed by agent
+  --framework.delay(5)                                       -- this delay is for reliability reasons
+  local avlStatesProperty = lsf.getProperties(avlAgentCons.avlAgentSIN,avlPropertiesPINs.avlStates)
+  -- assertion gives the negative result if terminal does not change the moving state to false
+  assert_false(avlHelperFunctions.stateDetector(avlStatesProperty).Moving, "terminal in the moving state")
+
+
+end
+-----------------------------------------------------------------------------------------------
+--- teardown function executed after each unit test
+function teardown()
+
+-- nothing here for now
+
+end
+
+--[[
+    START OF TEST CASES
+
+    Each test case is a global function whose name begins with "test"
+
+--]]
+
+--- THIS IS DRAFT VERSION
+-- this is only to check handling of the IOs and configuration of the ports
+function test_Ignition_WhenPortValueTogglesIgnitionOnMessageSent()
+
+  local gpsSettings={
+              speed = 0,  -- one kmh above threshold
+              latitude = 1,                   -- degrees
+              longitude = 1                   -- degrees
+                     }
+  -- setting the EIO properties
+  lsf.setProperties(avlAgentCons.EioSIN,{
+                                                {avlPropertiesPINs.port1Config, 3},     -- port 1 as digital input
+                                                {avlPropertiesPINs.port1EdgeDetect, 3}  -- for both rising and falling edge
+                                        }
+                   )
+  -- setting AVL PropertiesPINs
+  lsf.setProperties(avlAgentCons.avlAgentSIN,{
+                                                {avlPropertiesPINs.funcDigInp1, 2} -- for ignition
+                                             }
+                   )
+
+  device.setIO(1, 0) -- to make sure port has low state
+  framework.delay(3)
+  device.setIO(1, 1) -- that should trigger IgnitionOn
+
+  --IgnitionOn message expected
+  message = gateway.getReturnMessage(framework.checkMessageType(avlAgentCons.avlAgentSIN, messagesMINs.ignitionON))
+
+  gpsSettings.heading = 361   -- for stationary state
+
+  local expectedValues={
+                  gps = gpsSettings,
+                  messageName = "IgnitionOn",
+                  currentTime = os.time()
+                        }
+
+  avlHelperFunctions.reportVerification(message, expectedValues ) -- verification of the report fields
+
+  local avlStatesProperty = lsf.getProperties(avlAgentCons.avlAgentSIN,avlPropertiesPINs.avlStates)
+  assert_true(avlHelperFunctions.stateDetector(avlStatesProperty).IgnitionON, "terminal not in the IgnitionOn state")
+
+
+end
+
+
+
+--[[Start the tests]]
+for i=1, 1, 1 do     -- to check the reliability, will be removed
+  lunatest.run()
+end
+
+framework.printResults()
+
+
