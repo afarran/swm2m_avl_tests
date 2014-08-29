4cc34e852351cd8eadc9796e105b613eda856b34
ArturMalyszewicz <amalyszewicz@pl.sii.eu>
2014-08-29 12:20:11 +0000

    added digitalStatesNames table and EioSIN

--- AvlAgentCons.lua
+++ AvlAgentCons.lua
@@ -4,8 +4,13 @@
 avlAgentCons = {
                       avlAgentSIN = 126,
                       avlStateNames = {"InLPM", "onMainPower", "Speeding", "Moving", "Towing", "GPSJammed", "CellJammed", "Tamper", "AirCommunicationBlocked",
-                                      "Reserved", "SeatbeltViolation", "IgnitionON", "EngineIdling", "SM1Active", "SM2Active", "SM3Active", "SM4Active", "Geodwelling" }  -- table of states of agent
-                }
+                                      "Reserved", "SeatbeltViolation", "IgnitionON", "EngineIdling", "SM1Active", "SM2Active", "SM3Active", "SM4Active", "Geodwelling" },  -- table of states of agent
+                      digitalStatesNames = {"IgnitionON", "SeatbeltOFF", "SM1Active", "SM2Active", "SM3Active", "SM4Active" },
+                      EioSIN = 25,
+
+
+
+              }
 
 
 
