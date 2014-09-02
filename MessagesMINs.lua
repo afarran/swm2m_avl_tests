665294c40b1d49494a4fd13061d1fe0fff90c952
ArturMalyszewicz <amalyszewicz@pl.sii.eu>
2014-09-02 14:19:17 +0000

    added two new PINs idlingStart and idlingEnd

--- MessagesMINs.lua
+++ MessagesMINs.lua
@@ -2,10 +2,14 @@
 --
 messagesMINs = {
   ignitionON = 4,
+  ignitionOFF = 5,
 	movingStart = 6,
   movingEnd = 7,
   speedingStart = 8,
-  speedingEnd = 9
+  speedingEnd = 9,
+  idlingStart = 21,
+  idlingEnd = 41,
+
 	        			}
 return messagesMINs
 
