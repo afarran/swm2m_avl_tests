a5c2c2a67dde3ee1800f4633799626049459a872
ArturMalyszewicz <amalyszewicz@pl.sii.eu>
2014-09-30 11:07:27 +0000

    in TestIOModule added new TC: test_DigitalInput_WhenTerminalMovingAndPort2StateChangesFromLowToHigh_DigInp2HiMessageSent

--- TestIOModule.lua
+++ TestIOModule.lua
@@ -1990,7 +1990,6 @@ function test_DigitalInput_WhenTerminalMovingAndPort1StateChangesFromLowToHigh_D
 end
 
 
-
 --- TC checks if DigInp1Lo message is sent when port 1 state changes from high to low
   -- *actions performed:
   -- Configure port 1 as a digital input and set General Purpose as function for digital input line number 1
@@ -2055,6 +2054,72 @@ function test_DigitalInput_WhenTerminalMovingAndPort1StateChangesFromHighToLow_D
 
 
 end
+
+
+
+
+--- TC checks if DigInp2Hi message is sent when port 2 state changes from low to high
+  -- *actions performed:
+  -- Configure port 2 as a digital input and set General Purpose as function for digital input line number 2
+  -- simulate terminal moving and change state of digital port 2 from low to high; check if DigInp2Hi message
+  -- has been sent from terminal and report contains correct values of fields
+  -- *initial conditions:
+  -- terminal not in the moving state and not in the low power mode, gps read periodically with interval of
+  -- gpsReadInterval; all 4 ports in LOW state, terminal not in the IgnitionOn state
+  -- *expected results:
+  -- DigInp2Hi message sent when port changes state from low to high
+function test_DigitalInput_WhenTerminalMovingAndPort2StateChangesFromLowToHigh_DigInp2HiMessageSent()
+
+  -- properties values to be used in TC
+  local movingDebounceTime = 1          -- seconds
+  local stationarySpeedThld = 5         -- kmh
+
+
+  -- gpsSettings table to be sent to simulator
+  local gpsSettings={
+              speed = stationarySpeedThld + 10, -- to simulate terminal in moving state
+              latitude = 1,                     -- degrees
+              longitude = 1,                    -- degrees
+              fixType = 3,                      -- valid fix provided
+              heading = 90                      -- heading in degrees
+                     }
+
+  -- setting the EIO properties
+  lsf.setProperties(avlAgentCons.EioSIN,{
+                                                {avlPropertiesPINs.port2Config, 3},     -- port 2 as digital input
+                                                {avlPropertiesPINs.port2EdgeDetect, 3}, -- port 2 detection for both rising and falling edge
+                                        }
+                   )
+  -- setting AVL properties
+  lsf.setProperties(avlAgentCons.avlAgentSIN,{
+                                                {avlPropertiesPINs.funcDigInp2, avlAgentCons.funcDigInp.GeneralPurpose}, -- line number 2 set for General Purpose function
+                                                {avlPropertiesPINs.stationarySpeedThld, stationarySpeedThld},            -- stationarySpeedThld
+                                                {avlPropertiesPINs.movingDebounceTime, movingDebounceTime},              -- movingDebounceTime
+
+                                             }
+                   )
+  gps.set(gpsSettings)                                     -- applying gps settings to make terminal moving
+  framework.delay(movingDebounceTime+gpsReadInterval+3)    -- wait terminal gets moving state and MovingStart message is processed
+  gateway.setHighWaterMark()                               -- to get the newest messages
+  device.setIO(2, 1)                                       -- set port 2 to high level - that should trigger DigInp2Hi
+  framework.delay(3)                                       -- wait until message is processed
+
+  receivedMessages = gateway.getReturnMessages()           -- receiving all the messages
+  -- flitering received messages to find DigInp2Hi message
+  local filteredMessages = framework.filterMessages(receivedMessages, framework.checkMessageType(avlAgentCons.avlAgentSIN, messagesMINs.digitalInp2Hi))
+  assert_true(next(filteredMessages), "DigInp2Hi report not received")   -- checking if digitalInp2Hi message has been caught, if not assertion fails
+  digitalInp2HiMessage = filteredMessages[1]                             -- that is due to structure of the filteredMessages
+  local expectedValues={
+                  gps = gpsSettings,
+                  messageName = "DigInp2Hi",
+                  currentTime = os.time(),
+                         }
+
+  avlHelperFunctions.reportVerification(digitalInp2HiMessage, expectedValues) -- verification of the report fields
+
+
+end
+
 
 
 
