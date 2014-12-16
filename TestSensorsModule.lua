57f376bf2989e44f4881a720c1fa246555d002cc
unknown <pblo28@gmail.com>
2014-12-16 09:50:19 +0100

    Refactoring of TC with SensorSimulator usage.

--- TestSensorsModule.lua
+++ TestSensorsModule.lua
@@ -6,13 +6,22 @@
 module("TestSensorsModule", package.seeall)
 
 local SENSOR_SERVICE_SIN = 128
-
+local GPS_SIN = 20
+   
 -- Setup and Teardown
 
 --- suite_setup
  -- suite_setup description
  
 function suite_setup()
+  
+  local CONTINUOUS_PIN = 15
+  
+  lsf.setProperties(lsfConstants.sins.position,{
+                                                {CONTINUOUS_PIN, 1 },
+                                               }
+                    )
+  framework.delay(2)
   
 end
 
@@ -44,34 +53,28 @@ end
 function test_PeriodicallySendingMessageContainingSensorValues()
   
     local SENSOR_REPORTING_INTERVAL = 1 -- 60 secs 
-    local SENSOR_SIMULATOR_SIN  = 128
-    local SENSOR_SIMULATOR_PIN  = 1
-    local SENSOR_SIMULATOR_DATA_MSG = 1
     local AVL_RESPONSE_MIN = 74
-    local SENSOR_1_EXPECTED_VALUE = 120
+    local SENSOR_1_EXPECTED_VALUE = 2
     local DEFAULT_TIMEOUT = 5*60
+    local GPS_LAT_PIN = 6;
     
     -- setting AVL properties
     lsf.setProperties(avlConstants.avlAgentSIN,{
                         {avlConstants.pins.SensorReportingInterval, SENSOR_REPORTING_INTERVAL},
-                        {avlConstants.pins.Sensor1Source, framework.base64Encode({SENSOR_SIMULATOR_SIN, SENSOR_SIMULATOR_PIN}), "data" }
+                        {avlConstants.pins.Sensor1Source, framework.base64Encode({GPS_SIN, GPS_LAT_PIN}), "data" }
                                              }
                     )
   
-    -- sending data to Simulator with expected value after 10th second
-    local message = {SIN = SENSOR_SIMULATOR_SIN, MIN = SENSOR_SIMULATOR_DATA_MSG }
-    message.Fields = {{Name="TimeChanges", Elements={{Index=0, Fields={{Name="time",Value=0 }, {Name="value",Value=11 }}},
-                                                  {Index=1, Fields={{Name="time",Value=5 }, {Name="value",Value=10 }}},
-                                                  {Index=2, Fields={{Name="time",Value=10 }, {Name="value",Value=SENSOR_1_EXPECTED_VALUE }}},
-                                                  }},}
-                                          
-    gateway.submitForwardMessage(message)
-    gateway.setHighWaterMark()         -- to get the newest messages
+    -- set monitored value in position service
+    gps.set({  speed = 1, heading = 90, latitude = 1, longitude = 1})
     
     -- waiting for periodical report 1
     local expectedMins = {AVL_RESPONSE_MIN}
     local receivedMessages = avlHelperFunctions.matchReturnMessages(expectedMins, DEFAULT_TIMEOUT+5)
     
+    -- set monitored value in position service to expected value
+    gps.set({  speed = 1, heading = 90, latitude = SENSOR_1_EXPECTED_VALUE, longitude = 2})
+
     local startTime = os.time()
     
     -- waiting for periodical report 2
@@ -81,46 +84,35 @@ function test_PeriodicallySendingMessageContainingSensorValues()
     local timeDiff = os.time() - startTime
     assert_equal(timeDiff , 60, 5, "Sensor Reporting Interval test failed - wrong timeout between messages")
     
-    -- checking if raported value is set properly
+    -- checking if raported value is monitored properly
     assert_equal(SENSOR_1_EXPECTED_VALUE , tonumber(receivedMessages[AVL_RESPONSE_MIN].Sensor1), 0, "Sensor Reporting Interval test failed - wrong expected value")
 
 end
 
 -- TC for seting single value of sensor 1
 function test_SettingSensorValue()
-    local SENSOR_SIMULATOR_SIN  = 128
-    local SENSOR_SIMULATOR_PIN  = 1
-    local SENSOR_SIMULATOR_DATA_MSG = 1
-    local SENSOR_1_EXPECTED_VALUE = 120
+    local SENSOR_1_EXPECTED_VALUE = 12
+    local GPS_LAT_PIN = 6;
     
     -- setting AVL properties
     lsf.setProperties(avlConstants.avlAgentSIN,{
-                        {avlConstants.pins.Sensor1Source, framework.base64Encode({SENSOR_SIMULATOR_SIN, SENSOR_SIMULATOR_PIN}), "data" }
+                        {avlConstants.pins.Sensor1Source, framework.base64Encode({GPS_SIN, GPS_LAT_PIN}), "data" }
                                              }
                     )
   
-    -- sending data to Simulator with expected value after 10th second
-    local message = {SIN = SENSOR_SIMULATOR_SIN, MIN = SENSOR_SIMULATOR_DATA_MSG }
-    message.Fields = {{Name="TimeChanges", Elements={
-                                                  {Index=0, Fields={{Name="time",Value=0 }, {Name="value",Value=11 }}},
-                                                  {Index=1, Fields={{Name="time",Value=1 }, {Name="value",Value=10 }}},
-                                                  {Index=2, Fields={{Name="time",Value=3 }, {Name="value",Value=SENSOR_1_EXPECTED_VALUE }}},
-                                                  }},}
-   
-    gateway.submitForwardMessage(message)
-    gateway.setHighWaterMark()         -- to get the newest messages
-    
-    framework.delay(4)
+    gps.set({  speed = 1, heading = 90, latitude = SENSOR_1_EXPECTED_VALUE, longitude = 2})
+
+    framework.delay(3)
     
     --verify properties
-    currentProperties = avlHelperFunctions.propertiesToTable(lsf.getProperties(SENSOR_SIMULATOR_SIN, {SENSOR_SIMULATOR_PIN,}))
+    currentProperties = avlHelperFunctions.propertiesToTable(lsf.getProperties(GPS_SIN, {GPS_LAT_PIN,}))
     
-    --print(framework.dump(currentProperties))
+    print(framework.dump(currentProperties))
     
-    sensor1Value = tonumber(currentProperties[1])
+    --sensor1Value = tonumber(currentProperties[1])
     
     --checking if raported value is set properly
-    assert_equal(SENSOR_1_EXPECTED_VALUE , sensor1Value , 0, "Sensor Value set - wrong expected value")
+    --assert_equal(SENSOR_1_EXPECTED_VALUE , sensor1Value , 0, "Sensor Value set - wrong expected value")
 end
 
 
